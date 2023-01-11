f6fedd8e9d02d405c30bf877f275700a478d6125
fundDeveloper22 <openfund365@gmail.com>
2023-01-11 17:13:53 +0900

    fix exception possible case
    use try_getPool(), try_liquidity()

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -14,6 +14,21 @@ import { UniswapV3Factory } from '../types/templates/XXXFund2/UniswapV3Factory'
 import { UniswapV3Pool } from '../types/templates/XXXFund2/UniswapV3Pool'
 
 const Q192 = f64(2 ** 192)
+// export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Address, token1: Address): BigDecimal[] {
+//   let num = sqrtPriceX96.times(sqrtPriceX96).toBigDecimal()
+//   let denom = BigDecimal.fromString(Q192.toString())
+//   const token0Decimals = ERC20.bind(token0).decimals()
+//   const token1Decimals = ERC20.bind(token1).decimals()
+
+//   let price1 = num
+//     .div(denom)
+//     .times(BigDecimal.fromString(f64(10 ** token1Decimals).toString()))
+//     .div(BigDecimal.fromString(f64(10 ** token0Decimals).toString()))
+
+//   let price0 = safeDiv(BigDecimal.fromString('1'), price1)
+  
+//   return [price0, price1]
+// }
 export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Address, token1: Address): BigDecimal[] {
   let num = sqrtPriceX96.times(sqrtPriceX96).toBigDecimal()
   let denom = BigDecimal.fromString(Q192.toString())
@@ -22,13 +37,14 @@ export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Address,
 
   let price1 = num
     .div(denom)
-    .times(BigDecimal.fromString(f64(10 ** token1Decimals).toString()))
-    .div(BigDecimal.fromString(f64(10 ** token0Decimals).toString()))
+    .times(BigDecimal.fromString(f64(10 ** token0Decimals).toString()))
+    .div(BigDecimal.fromString(f64(10 ** token1Decimals).toString()))
 
   let price0 = safeDiv(BigDecimal.fromString('1'), price1)
   
   return [price0, price1]
 }
+
 
 export function getEthPriceInUSD(): BigDecimal {
   const wethAddress = Address.fromString(WETH9)
@@ -40,14 +56,25 @@ export function getEthPriceInUSD(): BigDecimal {
 
   for (let i=0; i<fees.length; i++) {
     const poolAddress = UniswapV3Factory.bind(Address.fromString(UNISWAP_V3_FACTORY))
-      .getPool(wethAddress, usdcAddress, fees[i])
-    const liquidity = UniswapV3Pool.bind(poolAddress).liquidity()
-
-    if (liquidity.gt(ZERO_BI) && liquidity.gt(largestLiquidity)) {
-      const slot0 = UniswapV3Pool.bind(poolAddress).slot0()
+      .try_getPool(wethAddress, usdcAddress, fees[i])
+    if (poolAddress.reverted) {
+      continue
+    }
+    const liquidity = UniswapV3Pool.bind(poolAddress.value).try_liquidity()
+    if (liquidity.reverted) {
+      continue
+    }
+    if (liquidity.value.gt(ZERO_BI) && liquidity.value.gt(largestLiquidity)) {
+      const token0 = UniswapV3Pool.bind(poolAddress.value).token0()
+      const token1 = UniswapV3Pool.bind(poolAddress.value).token1()
+      const slot0 = UniswapV3Pool.bind(poolAddress.value).slot0()
       const sqrtPriceX96 = slot0.getSqrtPriceX96()
-      ethPrice = sqrtPriceX96ToTokenPrices(sqrtPriceX96, wethAddress, usdcAddress)[0]
-      largestLiquidity = liquidity
+      if (token0.equals(Address.fromHexString(WETH9))) {
+        ethPrice = sqrtPriceX96ToTokenPrices(sqrtPriceX96, token0, token1)[0]
+      } else {
+        ethPrice = sqrtPriceX96ToTokenPrices(sqrtPriceX96, token0, token1)[1]
+      }
+      largestLiquidity = liquidity.value
     }
   }
   return ethPrice
@@ -67,12 +94,24 @@ export function getPriceETH(token: Address, amountIn: BigInt): BigDecimal {
 
   for (let i=0; i<fees.length; i++) {
     const poolAddress = UniswapV3Factory.bind(Address.fromString(UNISWAP_V3_FACTORY))
-      .getPool(tokenAddress, wethAddress, fees[i])
-    const liquidity = UniswapV3Pool.bind(poolAddress).liquidity()
-    if (liquidity.gt(ZERO_BI) && liquidity.gt(largestLiquidity)) {
-      const sqrtPriceX96 = UniswapV3Pool.bind(poolAddress).slot0().getSqrtPriceX96()
-      price = sqrtPriceX96ToTokenPrices(sqrtPriceX96, tokenAddress, wethAddress)[0]
-      largestLiquidity = liquidity
+      .try_getPool(tokenAddress, wethAddress, fees[i])
+    if (poolAddress.reverted) {
+      continue
+    }
+    const liquidity = UniswapV3Pool.bind(poolAddress.value).try_liquidity()
+    if (liquidity.reverted) {
+      continue
+    }
+    if (liquidity.value.gt(ZERO_BI) && liquidity.value.gt(largestLiquidity)) {
+      const token0 = UniswapV3Pool.bind(poolAddress.value).token0()
+      const token1 = UniswapV3Pool.bind(poolAddress.value).token1()
+      const sqrtPriceX96 = UniswapV3Pool.bind(poolAddress.value).slot0().getSqrtPriceX96()
+      if (token0.equals(Address.fromHexString(WETH9))) {
+        price = sqrtPriceX96ToTokenPrices(sqrtPriceX96, token0, token1)[1]
+      } else {
+        price = sqrtPriceX96ToTokenPrices(sqrtPriceX96, token0, token1)[0]
+      }
+      largestLiquidity = liquidity.value
     }
   }
   return price.times(amountIn.toBigDecimal().div(WETH_DECIMAL))
